454b4533a7be5eb37dea163001f99408532c5e89
NeemaHuru <neemahuru@gmail.com>
2023-07-20 04:49:32 -0700

    98 battery street,the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,6 +5,7 @@
  * print_to_98 - prints all natural numbers from n to 98,
  * followed by a new line
  * @n: print from this number
+ * Return: void 
  */
 void print_to_98(int n)
 {
@@ -12,21 +13,22 @@ void print_to_98(int n)
 
 	if (n <= 98)
 	{
-	for (i = n; i <= 98; i++)
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
+		}
+	} 
+	else if (n > 98)
 	{
-		if (i != 98)
-			printf("% d,", i);
-		else if (i == 98)
-			printf(" % d\n", i);
+		for (j = n; j >= 98; j--)
+		{
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
+		}
 	}
-} else if (n >= 98)
-{
-	for (j = n; j >= 98; j--)
-	{
-		if (j != 98)
-			printf(" % d,", j);
-		else if (j == 98)
-			printf("% d\n", j);
-	}
-}
 }
