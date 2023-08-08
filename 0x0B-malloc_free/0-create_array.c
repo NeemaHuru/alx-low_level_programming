0b3cab420406a137e5a2be000b4d02427a05aea4
NeemaHuru <neemahuru@gmail.com>
2023-08-08 15:59:38 -0700

    The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * *create_array - creates an array of chars
+ * @size: of array
+ * @c: char
+ * Return: pointer to the array, or NULL if it fails
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a = malloc(size * sizeof(char));
+	unsigned int i;
+
+	if (size == 0 || a == 0)
+		return (NULL);
+	for (i = 0 ; i < size ; i++)
+		a[i] = c;
+	return (a);
+}
